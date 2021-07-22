8ae0190c7e4689497af275d4f959ea1ac4a7ad05
glu <glusooner@gmail.com>
2021-07-22 18:24:16 -0500

    change git default editor to vim

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def hello(name):
     print(f'this is hello from linux: {name}')
 
 def hello2(name):
-    print('hello. hello. hello')
+    print('hello. hello. hello.hello')
 
 hello('george')
 hello2('george')
