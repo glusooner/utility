3f0691abdf3b69695eb21cb71616ae103da011a8
glu <glusooner@gmail.com>
2021-07-30 08:00:23 -0500

    test cron

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 from util import *
+import datetime as dt
 
 def hello(name):
-    print(f'this is hello from linux: {name}')
+	f=open("log.txt","a")
 
+	f.write("this line is expected to appear every min\n")
+	f.write(dt.datetime.now().strftime("%Y%m%d %H%M%S"))
+	f.write('\n')
+	f.close()
+	print('job done')
 def hello2(name):
     print('hello. hello. hello.hello')
 
 hello('george')
-hello2('george')
-hello3()
