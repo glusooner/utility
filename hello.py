5cc17531e146f47a29aafe1b63b10fd40cdd585c
georgelu <glusooner@gmail.com>
2023-06-21 21:25:28 -0500

    fix format

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
 from util import *
 import datetime as dt
 
-def hello(name):
-	f=open("log.txt","a")
 
-	f.write("this line is expected to appear every min\n")
-	f.write(dt.datetime.now().strftime("%Y%m%d %H%M%S"))
-	f.write('\n')
-	f.close()
-	print('job done')
+def hello(name):
+    f = open("log.txt", "a")
+
+    f.write("this line is expected to appear every min\n")
+    f.write(dt.datetime.now().strftime("%Y%m%d %H%M%S"))
+    f.write('\n')
+    f.close()
+    print('job done')
+
+
 def hello2(name):
     print('hello. hello. hello.hello')
+
 
 hello('george')
